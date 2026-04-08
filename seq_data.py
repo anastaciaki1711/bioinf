2cc37c1b3338eb691b45536eda1483a54952d95a
anastaciaki1711 <debilyfc9@gmail.com>
2026-04-09 00:31:59 +0300

    вывод названия последовательности(первая строка)

--- seq_data.py
+++ seq_data.py
@@ -1,8 +1,13 @@
+a=open(r"C:\Users\Настя\Downloads\rosalind_gc.txt", "r")
 class Seq():
     def __init__(self,file):
         self.file=file
     def my_sequence(self):
-        return print(self.file.read())
-a=open(r"C:\Users\Настя\Downloads\rosalind_gc.txt", "r")
+        self.file.seek(0)
+        first_line=self.file.readline()
+        if first_line.startswith ('>'):
+            first_line=first_line[1:]
+        return print(first_line)
+
 result=Seq(a)
-result.my_sequence()
+id=result.my_sequence()
