55827b1ddc517058782c89550abf463cca11ea7e
anastaciaki1711 <debilyfc9@gmail.com>
2026-04-09 00:43:45 +0300

    вывод последовательности

--- seq_data.py
+++ seq_data.py
@@ -2,12 +2,17 @@ a=open(r"C:\Users\Настя\Downloads\rosalind_gc.txt", "r")
 class Seq():
     def __init__(self,file):
         self.file=file
-    def my_sequence(self):
+    def my_sequence_id(self):
         self.file.seek(0)
         first_line=self.file.readline()
         if first_line.startswith ('>'):
             first_line=first_line[1:]
         return print(first_line)
-
+    def my_sequence(self):
+        k=self.file.read()
+        line=k.splitlines()
+        second_line= "\n".join(line[1:])
+        return print(second_line)
 result=Seq(a)
-id=result.my_sequence()
+id=result.my_sequence_id()
+sequence=result.my_sequence()
