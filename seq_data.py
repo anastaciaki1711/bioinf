d67adac90689fdb101c3015bb3e860be210c7523
anastaciaki1711 <debilyfc9@gmail.com>
2026-04-08 22:04:05 +0300

    тест:вывод строки из класса

--- seq_data.py
+++ seq_data.py
@@ -1,2 +1,8 @@
-class Seq:
-    pass
+class Seq():
+    def __init__(self,file):
+        self.file=file
+    def my_sequence(self):
+        return print(self.file.read())
+a=open(r"C:\Users\Настя\Downloads\rosalind_gc.txt", "r")
+result=Seq(a)
+result.my_sequence()
